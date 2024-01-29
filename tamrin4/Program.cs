e73f021ab052ca5bc0c436702d66cf1b0e306868
User <User@DESKTOP-1364VBG>
2024-01-29 17:48:06 +0330

    Add project files.

---
+++ tamrin4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace project
+{
+    class test
+    {
+        public static void Main()
+        {
+            double qiz = Convert.ToDouble(Console.ReadLine()); 
+            double final = Convert.ToDouble(Console.ReadLine());
+            name(qiz, final);
+        }
+        public static void name (double qiz,double final)
+        {
+            double volume = 0 / 35 * qiz + 0 / 65 * final;
+            Console.WriteLine(volume);
+        }
+    }
+}
